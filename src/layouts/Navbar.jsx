9826248d322ef944a970cb881706678ecd8d022e
akmlrafly <akmalrafly875@gmail.com>
2025-03-07 14:32:40 +0700

    add hero section + navbar

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="w-full flex justify-center pt-5 absolute">
       <div className="w-6xl flex justify-between items-center px-12 py-5 rounded-xl shadow-md bg-white/60 backdrop-blur-sm">
-        <h1 className="font-semibold text-md text-sky-950">MySkin</h1>
+        <h1 className="font-semibold text-md text-blue-950">MySkin</h1>
         <div className="flex gap-x-4">
           <ListNavbar>Beranda</ListNavbar>
           <ListNavbar>FAQ</ListNavbar>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,7 +6,9 @@ const Homepage = () => {
   return (
     <div className="w-full h-screen relative">
       <Navbar />
-      <div className="w-full h-[80%]">
+
+      {/* Hero Section */}
+      <div className="w-full h-[70%]">
         <img
           src={bgHomepage}
           alt="Dokter"
@@ -14,17 +16,19 @@ const Homepage = () => {
         />
 
         {/* Text & Button */}
-        <div className="absolute left-32 top-1/3 max-w-md">
-          <h1 className="text-5xl font-light text-gray-800">
+        <div className="absolute left-32 top-1/4 max-w-md">
+          <h1 className="text-5xl font-light text-sky-900">
             Selamat Datang, <br />
             <span className="font-bold">Muhammad</span>
           </h1>
-          <button className="mt-6 flex items-center gap-2 bg-white text-sky-900 font-semibold px-6 py-3 rounded-full shadow-lg cursor-pointer hover:bg-sky-100">
+          <button className="mt-6 flex items-center gap-2 bg-white text-blue-900 font-semibold px-6 py-3 rounded-full shadow-lg cursor-pointer hover:bg-blue-100">
             <span className="material-icons"><img src={scan} alt=""/></span>
             Coba Sekarang
           </button>
         </div>
       </div>
+
+      {/*  */}
     </div>
   );
 }
