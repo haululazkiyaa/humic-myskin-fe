e34a207af7e6411a46ae256106adb84bca618abc
akmlrafly <akmalrafly875@gmail.com>
2025-03-20 14:54:51 +0700

    add responsive hero-section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,7 +7,7 @@ const Homepage = () => {
   return (
     <div className="w-full min-h-screen">
       {/* Hero Section */}
-      <div className="w-full h-[32rem]">
+      <div className="w-full h-80 md:h-[32rem]">
         <img
           src={bgHomepage}
           alt="Dokter"
@@ -15,14 +15,14 @@ const Homepage = () => {
         />
 
         {/* Text & Button */}
-        <div className="absolute left-32 top-48 max-w-md">
-          <h1 className="text-5xl font-light text-sky-900">
+        <div className="absolute left-8 md:left-32 top-28 md:top-48 max-w-md">
+          <h1 className="text-4xl md:text-5xl font-light text-sky-900">
             Selamat Datang, <br />
             <span className="font-bold">Muhammad</span>
           </h1>
           <button
             onClick={() => (window.location.href = "#deteksi")}
-            className="mt-6 flex items-center gap-2 bg-white text-blue-900 font-semibold px-6 py-3 rounded-full shadow-lg cursor-pointer hover:bg-blue-100"
+            className="mt-4 md:mt-6 flex items-center gap-2 bg-white text-blue-900 font-semibold px-4 md:px-6 py-1 md:py-3 rounded-full shadow-lg cursor-pointer hover:bg-blue-100"
           >
             <span className="material-icons">
               <img src={scan} alt="" />
