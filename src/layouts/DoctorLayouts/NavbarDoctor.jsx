584dea950ca629eca0291839ea9b95d3175bb198
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2025-04-05 20:45:11 +0700

    feat: add mobile navbar

--- src/layouts/DoctorLayouts/NavbarDoctor.jsx
+++ src/layouts/DoctorLayouts/NavbarDoctor.jsx
@@ -3,16 +3,20 @@ import { Link, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import AuthForm from "../../components/form/AuthForm";
+import { FaBarsStaggered } from "react-icons/fa6";
+import ListNavbar from "../../components/ListNavbar";
 import useModal from "../../hooks/useModal";
 
 const Navbar = () => {
   const { Modal, onClose } = useModal();
   const location = useLocation();
   const [url, setUrl] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const currentPath = location.pathname.split("/")[2] || "dashboard";
     setUrl(currentPath);
+    setIsOpen(false);
   }, [location]);
 
   const handleLogout = () => {
@@ -42,8 +46,36 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="w-full flex justify-center">
-      <div className="w-full flex justify-between items-center px-12 py-5 border-b-[1px] border-[#E3E3E3] bg-white/60 backdrop-blur-sm">
+    <div className="w-full flex justify-center mb-[60px] lg:mb-0">
+      {/* navbar mobile */}
+      <div className="fixed w-full z-[20] bg-white">
+        <div className="flex lg:hidden w-full justify-between items-center px-6 py-5 border-b-[1px] border-[#E3E3E3]">
+          <div onClick={() => setIsOpen(!isOpen)}>
+            <FaBarsStaggered />
+          </div>
+          <h1 className="flex md:hidden font-bold mx-auto pr-[18px] text-lg text-blue-950">
+            MySkin
+          </h1>
+        </div>
+        {isOpen && (
+          <div className="absolute top-17 left-0 w-full flex flex-col items-center p-5 gap-y-5 shadow-lg bg-white">
+            {menuItems.map((item) => (
+              <ListNavbar key={item.key} to={item.to}>
+                {item.label}
+              </ListNavbar>
+            ))}
+            <button
+              onClick={handleLogout}
+              className="font-extralight text-md cursor-pointer text-red-500"
+            >
+              Keluar
+            </button>
+          </div>
+        )}
+      </div>
+
+      {/* navbar desktop */}
+      <div className="hidden lg:flex w-full justify-between items-center px-12 py-5 border-b-[1px] border-[#E3E3E3] bg-white/60 backdrop-blur-sm">
         <h1 className="font-semibold text-md text-blue-950">MySkin</h1>
         <div className="flex gap-x-12 items-center">
           {menuItems.map((item) => (
