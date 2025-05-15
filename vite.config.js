27e40f7ead264a064871335cca3289fe7f866ccf
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2025-05-15 12:26:00 +0700

    gitignore

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://myskin-v2.humicprototyping.com/",
   plugins: [react(), tailwindcss()],
 });
